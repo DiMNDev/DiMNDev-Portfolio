e070b4a48d8b0dc603be3aa0a8b87ae6849d2274
DiMNDev <Dimndev@gmail.com>
2024-01-09 21:29:54 -0700

    update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Joshua Arnold",
   description: "A NextJS Portfolio",
+  opengraph: {
+    url: "https://www.dimndev.com/",
+  },
 };
 
 export default function RootLayout({ children }) {
